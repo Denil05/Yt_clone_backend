9f74b7eb012391a743cf7939823c85981c33fbfc
Denil <187249628+Denil05@users.noreply.github.com.>
2025-06-11 21:37:59 +0530

    mongo atlaes connect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-ÿþ
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+connectDB();
+
+/* This first method to connect database*/
+// import express from "express";
+
+// const app = express();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR: ",error);
+//             throw error
+//         });
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listing on port ${process.env.PORT}`);
+//         })
+//     } catch(error) {
+//         console.error("ERROR: ",error);
+//         throw error;
+//     }
+// })() 
